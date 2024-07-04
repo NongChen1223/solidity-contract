a82f3b692065fcd193afa45d2d769bcf6f29940c
Xww <claire@chainingview.com>
2024-07-04 17:03:22 +0800

    build: 更新配置项引入新升级合约依赖

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # solidity-contract
 Individuals issue test symbol
 
+# .env
+.env文件中的PRIVATE_KEY 放置你要发布的钱包地址的私钥
+
 ## 构建项目
 ```
  pnpm add -D hardhat
  pnpm hardhat init
+```
+## 发布流程
+```
+npx hardhat compile 编译contracts下的sol
+npx hardhat run scripts/t1.deploy.ts --network bscTestnet 发布对应的代币
 ```
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    bscTestnet: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
 };
 
 export default config;
