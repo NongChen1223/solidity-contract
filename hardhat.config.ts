ec96a4c5a6b766b4c79d918b125087e92fcd9e6a
Xww <claire@chainingview.com>
2024-07-10 15:57:08 +0800

    pref: 新增代理发布

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,16 +3,28 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
 import "@openzeppelin/hardhat-upgrades";
+import { ProxyAgent, setGlobalDispatcher } from "undici";
 import * as dotenv from "dotenv";
 
 dotenv.config();
+
+// 设置代理
+const proxyAgent = new ProxyAgent("http://127.0.0.1:1080");
+setGlobalDispatcher(proxyAgent);
+
 const config: HardhatUserConfig = {
 	solidity: "0.8.24",
 	networks: {
 		bscTestnet: {
-			url: `https://endpoints.omniatech.io/v1/bsc/testnet/public/`,
+			url: "https://go.getblock.io/3faf56936fe140e1beadbe0711b28f33/",
 			chainId: 97,
-			gasPrice: 100000,
+			gasPrice: 10000000,
+			accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+		},
+		opbnbTestnet: {
+			url: "https://opbnb-testnet-rpc.bnbchain.org/",
+			chainId: 5611,
+			gasPrice: 10000000,
 			accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
 		},
 	},
