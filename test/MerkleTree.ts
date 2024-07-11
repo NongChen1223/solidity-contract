4b2a027c85f33740c04fd275d46911b5916ac388
Xww <claire@chainingview.com>
2024-07-11 18:06:53 +0800

    test: 更改测试文件

--- test/MerkleTree.ts
+++ test/MerkleTree.ts
@@ -1,70 +1,36 @@
 import { MerkleTree } from "merkletreejs";
 import keccak256 from "keccak256";
 import dotenv from "dotenv";
-import { createMerkleTreeHexRoot, verifyProof } from "../tools/merkleTree";
+import { createMerkleTreeHexRoot, verifyProof, bufferConcat } from "../tools/merkleTree";
 dotenv.config();
-
 type WhitelistEntry = {
 	address: string;
 	amount: number;
 };
 // 示例字符串列表
-const strings = ["string1", "string2", "string3"];
-const WHITES = [
-	"0x7aa2B38737F958d9F7f47C27aD0807d61e66f7fb|10",
-	"0x64913142f4528DB3eAdA8A8490a96b7d3bbF1d63|30",
-	"0x55f0A57CB98fb58FAc37bdAeCb1cb0E88aB69c27|40",
-	"0xBa557Fc8D40A2eCa08ddD9B1F95F625d5A350189|50",
-	"0xbe8Da62D6a8Cd7C01236E2bCFe9C7f06A483Dd00|60",
-	"0xDF24BDC333d1AD7aDB057DD03627ad663c90CB77|100",
+const WHITES_V2 = [
+	{ address: "0x7aa2B38737F958d9F7f47C27aD0807d61e66f7fb", amount: 10 },
+	{ address: "0x64913142f4528DB3eAdA8A8490a96b7d3bbF1d63", amount: 20 },
+	{ address: "0x55f0A57CB98fb58FAc37bdAeCb1cb0E88aB69c27", amount: 30 },
+	{ address: "0xBa557Fc8D40A2eCa08ddD9B1F95F625d5A350189", amount: 40 },
+	{ address: "0xbe8Da62D6a8Cd7C01236E2bCFe9C7f06A483Dd00", amount: 50 },
+	{ address: "0xDF24BDC333d1AD7aDB057DD03627ad663c90CB77", amount: 60 },
 ];
-
-// 使用 keccak256 哈希函数为每个字符串创建默克尔树叶子
-function createLeaves(strings: string[]): Buffer[] {
-	return strings.map(str => keccak256(str));
-}
-
-async function createMerkleTreeAndVerify(list: string[]) {
-	// 创建叶子节点列表
-	const leaves = createLeaves(list);
-	console.log("校验参数", list);
-	// 创建默克尔树，使用 keccak256 哈希算法和叶节点排序功能
-	const merkleTree = new MerkleTree(leaves, keccak256, { sortPairs: true });
-
-	// 获取树的根哈希，通常会被存储在智能合约中用于验证
-	const rootHash = merkleTree.getHexRoot();
-	console.log("Merkle Root:", rootHash);
-
-	// 打印Merkle树结构
-	console.log("Merkle Tree:");
-	console.log(merkleTree.toString());
-
-	// 为每个字符串生成证明并进行验证
-	list.forEach((str, index) => {
-		const leaf = keccak256(str);
-		const proof = merkleTree.getHexProof(leaf);
-		const isValid = merkleTree.verify(proof, leaf, rootHash);
-		console.log("proof", proof);
-		console.log(`验证字符串 "${str}" 是否在Merkle树中:`, isValid);
-	});
-}
-
-async function testMerkleTree(list: string[]) {
+const proof = [];
+async function testMerkleTree(list: WhitelistEntry[]) {
 	const { merkleTree, rootHash } = createMerkleTreeHexRoot(list);
 	console.log("打印默克尔树");
 	console.log(merkleTree.toString());
-
 	console.log("打印根哈希", rootHash);
-	//校验节点
+	//循环测试校验节点
 	list.forEach((str, index) => {
-		const leaf = keccak256(str);
+		const leaf = keccak256(bufferConcat(str.address, str.amount));
 		const proof = merkleTree.getHexProof(leaf);
 		const isValid = verifyProof(merkleTree, proof, leaf, rootHash);
-		console.log(`打印${str}的leaf:`, leaf);
-		console.log(`打印${str}的proof:`, proof);
-		console.log(`验证字符串 "${str}" 是否在Merkle树中:`, isValid);
+		console.log(`打印${str.address}的leaf:`, leaf);
+		console.log(`打印${str.address}的proof:`, proof);
+		console.log(`验证字符串 "${str.address}" 是否在Merkle树中:`, isValid);
 	});
 }
 // 运行示例
-// createMerkleTreeAndVerify(WHITES);
-testMerkleTree(WHITES);
+testMerkleTree(WHITES_V2);
--- test/T1_airdop.ts
+++ test/T1_airdop.ts
@@ -5,14 +5,19 @@ dotenv.config();
 
 //空投合约地址
 const air_contractAddress = process.env.AIR_CONTRACT_ADDRESS_OPBNB; // 替换为你的合约地址
-const web3 = new Web3("https://opbnb-testnet-rpc.bnbchain.org/"); // 替换为你的以太坊节点地址
+const web3 = new Web3(process.env.WEB3_RPC_BSC); // 替换为你的以太坊节点地址
 // 获取合约实例
 const contract = new web3.eth.Contract(abi, air_contractAddress);
 //测试账户地址和私钥
 const account = "";
 const privateKey = "";
 //该账户的merkleProof
-const merkleProof = [];
+const merkleProof = [
+	"0xeb799d2f39646a8d839bfbed7911af66d4e6435ff216acfe5d12476e51639d1a",
+	"0x227ab06278e0962eed04e9bf6bc5864f1b16d7d1d00a56e58c0e245f37b198f5",
+	"0x55ba317728fc52bdb549d5dc4c12ce61df44b811915f2a619d0dc7c64ca76729",
+];
+
 //拼接
 const amount = 10; // 替换为你的空投代币数量
 const accountJoin = `${account}|${amount}`; // 替换为你的account_join字符串
